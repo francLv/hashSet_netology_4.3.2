8a5bab7f9d02394909896f34850e71d1ccc449ea
Владимир Левин <levin_vlsdimir@mail.ru>
2022-02-28 22:18:46 +0300

    Initial commit

---
+++ src/ru/netology/Student.java
@@ -0,0 +1,34 @@
+package ru.netology;
+
+public class Student {
+    private String name;
+    private String group;
+    private String studentId;
+
+    public Student(String name, String group, String studentId) {
+        this.name = name;
+        this.group = group;
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", group='" + group + '\'' +
+                ", studentId='" + studentId + '\'' +
+                '}';
+    }
+}
